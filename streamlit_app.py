0996c8f9431c15fa999a5ed6a9b113ff20178dd2
saintPablo28 <104412005+saintPablo28@users.noreply.github.com>
2022-10-01 10:40:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import numpy as np
 
 Teacher Dashboard
 
-Student Responses
+
 """
 
 
@@ -19,8 +19,8 @@ Student Responses
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    total_points = st.slider("Number of AI Practice Questions", 1, 5000, 2000)
+    num_turns = st.slider("Difficulty of Practice Set", 1, 100, 9)
 
     Point = namedtuple('Point', 'x y')
     data = []
@@ -31,8 +31,8 @@ with st.echo(code_location='below'):
         curr_turn, i = divmod(curr_point_num, points_per_turn)
         angle = (curr_turn + 1) * 2 * 2 * i / points_per_turn
         radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
+        x = radius * np.random.randint(1,3)
+        y = radius * np.random.randint(1,3)
         data.append(Point(x, y))
 
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
