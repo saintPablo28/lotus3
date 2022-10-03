4051f620962a716763467470dd5f4c1baefbb83a
saintPablo28 <104412005+saintPablo28@users.noreply.github.com>
2022-10-03 00:22:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,8 +65,8 @@ st.line_chart(chart_data)
     
 st.write("Raw Student Response Data")
 dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % 2)
+    np.random.randn(10, 20)
+    columns=('col %d' % i for i in range(20)))
 st.table(dataframe)
 
 
